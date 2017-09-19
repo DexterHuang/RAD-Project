dfd5e6e00985d3be7111b570d5153d9b4123991f
DexterHuang <ching.huang.dexter@gmail.com>
2017-09-19 21:39:58 +0800

    added button at home page to direct to posting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,18 @@
+import { PostRequestPageComponent } from './post-request-page/post-request-page/post-request-page.component';
+import { PostTripPageComponent } from './post-trip-page/post-trip-page/post-trip-page.component';
 import { MenuPageComponent } from './menu-page/menu-page/menu-page.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomePageComponent } from "./home-page/home-page/home-page.component";
-import { ContactPageComponent } from "./contact-page/contact-page/contact-page.component";
+import { HomePageComponent } from './home-page/home-page/home-page.component';
+import { ContactPageComponent } from './contact-page/contact-page/contact-page.component';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'home', component: HomePageComponent },
   { path: 'contact', component: ContactPageComponent },
-  { path: 'menu', component: MenuPageComponent }
+  { path: 'menu', component: MenuPageComponent },
+  { path: 'postTrip', component: PostTripPageComponent },
+  { path: 'postRequest', component: PostRequestPageComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { SuiModule } from 'ng2-semantic-ui';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page/home-page.component';
 import { NavbarComponent } from './common/navbar/navbar.component';
 import { ContactPageComponent } from './contact-page/contact-page/contact-page.component';
 import { MenuPageComponent } from './menu-page/menu-page/menu-page.component';
+import { PostTripPageComponent } from './post-trip-page/post-trip-page/post-trip-page.component';
+import { PostRequestPageComponent } from './post-request-page/post-request-page/post-request-page.component';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
     NavbarComponent,
     ContactPageComponent,
-    MenuPageComponent
+    MenuPageComponent,
+    PostTripPageComponent,
+    PostRequestPageComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule, FormsModule,
+    AppRoutingModule,
+    SuiModule
   ],
   providers: [],
   bootstrap: [AppComponent]
